0ac48c44c703dc747c7bc37b1f4ebc8f01a13270
anwaar29 <anwaaransari29@gmail.com>
2022-05-25 00:08:11 -0700

    lll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Work from './Work';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // pure componeet
+  <>
+  <h1 style={{textAlign:'center'}} >React Pure Function ||||  &&&& ||||    React Higher Order Function </h1>
+  <pre style={{textAlign:'center',fontSize:'22px'}}>
+  A React component is considered pure if it renders the same output for the same state and props. <br/>For this type of class component, React provides the PureComponent base class. Class components that <br/>extend the React. PureComponent class are treated as pure components.
+  <Work />
+  </pre>
+  
+
+
+
+
+  {/* Higher order function */}
+  <pre style={{textAlign:'center',fontSize:'22px'}}>
+  A higher order component function accepts another function as an argument. The map function is the <br/>best example to understand this. The main goal of this is to decompose the component logic into simpler<br/> and smaller functions that can be reused as you need.
+  </pre>
+  </>
   );
 }
 
